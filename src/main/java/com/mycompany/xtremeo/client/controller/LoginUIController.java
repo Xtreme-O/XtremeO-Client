6f105f3d345d0a5ebcf35c82eda23104d91c3580
aeinae <aliabdalkareem.ak2000@gmail.com>
2026-01-08 02:50:35 +0200

    created login-ui screen

---
+++ src/main/java/com/mycompany/xtremeo/client/controller/LoginUIController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.xtremeo.client.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+/**
+ *
+ * @author LOQ
+ */
+public class LoginUIController {
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+    
+
+}
