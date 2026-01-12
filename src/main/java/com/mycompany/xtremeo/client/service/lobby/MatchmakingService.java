d5a07293db41c093edc15b9c434c8af277d4ee33
Wahid-Qandil <wahidqandill@gmail.com>
2026-01-12 19:47:56 +0200

    Implement equals and hashCode in Player, update matchmaking
    Added equals and hashCode methods to the Player class for proper object comparison. Updated MatchmakingService to handle invite responses using the new equality logic and refactored challenge acceptance to use confirmInvite instead of sendInvite.

--- src/main/java/com/mycompany/xtremeo/client/service/lobby/MatchmakingService.java
+++ src/main/java/com/mycompany/xtremeo/client/service/lobby/MatchmakingService.java
@@ -3,6 +3,7 @@ package com.mycompany.xtremeo.client.service.lobby;
 import com.mycompany.xtremeo.client.data.DataProvider;
 import com.mycompany.xtremeo.client.model.common.Player;
 import com.mycompany.xtremeo.client.network.ClientConnection;
+import com.mycompany.xtremeo.client.protocol.handler.game.InviteResponseHandler;
 import com.mycompany.xtremeo.client.service.SocketRequestSender;
 import com.mycompany.xtremeo.client.service.game.InviteService;
 import com.mycompany.xtremeo.client.util.GsonProvider;
@@ -20,6 +21,20 @@ public class MatchmakingService {
     private Runnable onMatchFound;
 
     private MatchmakingService() {
+        InviteResponseHandler.setOnInviteResponseConsumer(e -> {
+            PlayerService service = PlayerService.getInstance();
+
+            Player currentPlayer = service.getCurrentPlayer();
+            Player player1 = e.player1();
+            Player player2 = e.player2();
+            
+            if (currentPlayer.equals(player1)) {
+                receiveChallenge(e.player2());
+            }
+            else if (currentPlayer.equals(player2)) {
+                receiveChallenge(e.player1());
+            }
+        });
     }
 
     public static MatchmakingService getInstance() {
@@ -47,6 +62,7 @@ public class MatchmakingService {
         if (onChallengeReceived != null) {
             onChallengeReceived.accept(from);
         }
+
     }
 
     public void simulateDemoChallenge() {
@@ -54,15 +70,17 @@ public class MatchmakingService {
     }
 
     public void acceptChallenge(Player player) {
+        PlayerService service = PlayerService.getInstance();
+        Player currentPlayer = service.getCurrentPlayer();
+        InviteService inviteService = new InviteService(new SocketRequestSender(GsonProvider.getGsonProvider(), ClientConnection.getInstance()));
+        inviteService.confirmInvite(currentPlayer.getId(), player.getId());
         System.out.println("Challenge accepted from: " + player.getUsername());
+        
+        
         pendingChallenges.remove(player);
         if (onPendingChallengesChanged != null) {
             onPendingChallengesChanged.accept(pendingChallenges);
         }
-        PlayerService service = PlayerService.getInstance();
-        Player currentPlayer = service.getCurrentPlayer();
-        InviteService inviteService = new InviteService(new SocketRequestSender(GsonProvider.getGsonProvider(), ClientConnection.getInstance()));
-        inviteService.sendInvite(currentPlayer, player);
 
     }
 
@@ -82,7 +100,7 @@ public class MatchmakingService {
         Player currentPlayer = service.getCurrentPlayer();
         InviteService inviteService = new InviteService(new SocketRequestSender(GsonProvider.getGsonProvider(), ClientConnection.getInstance()));
         inviteService.sendInvite(currentPlayer, player);
-        
+
     }
 
     public void startMatchmaking() {
